0e155d3ce93de982361247b931eca1c5f8a188a0
Jeremy Tran Perez <jeremytranperez@gmail.com>
2026-04-08 16:43:14 -0400

    change Contact button colours

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -8,14 +8,14 @@ export default function Contact() {
     <section id="contact" className="scroll-m-2 bg-black p-10 text-white">
       <div className="flex flex-col items-center justify-center">
         <h1 className="font-lexend text-center text-5xl font-semibold">Contact</h1>
-        <div className="my-10 flex flex-col md:flex-row">
-          <p className="m-10 max-w-100 text-center text-xl md:text-left">For any questions or inquiries please feel free to contact me at:</p>
-          <div className="flex flex-col gap-5">
-            <div className="font-lexend flex items-center justify-between gap-5 rounded-2xl bg-gray-800 p-5">
+        <div className="my-10 flex flex-col items-center justify-center">
+          <p className="m-10 max-w-100 text-center text-xl">For any questions or inquiries please feel free to contact me at:</p>
+          <div className="flex w-full max-w-100 flex-col gap-5">
+            <div className="font-lexend flex items-center justify-between gap-5 rounded-2xl bg-white p-5 text-black">
               <SMSIcon className="h-10 w-10" />
               <span>(519) 329-4982</span>
             </div>
-            <div className="font-lexend flex items-center justify-between gap-5 rounded-2xl bg-gray-800 p-5">
+            <div className="font-lexend 300 flex items-center justify-between gap-5 rounded-2xl bg-white p-5 text-black transition-colors ease-in-out">
               <EmailIcon className="h-10 w-10" />
               <a href="mailto:kevintran784@gmail.com">
                 <span>kevintran784@gmail.com</span>
