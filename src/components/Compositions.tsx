1975533ea66bb498a9188ce2d21d409d185a4c16
Jeremy Tran Perez <jeremytranperez@gmail.com>
2026-04-11 19:28:07 -0400

    added PDF for Introduction to Music History

--- src/components/Compositions.tsx
+++ src/components/Compositions.tsx
@@ -1,7 +1,6 @@
 import "../styles/index.css";
 import { CompositionData, type Experience as ExperienceType } from "../constants";
-import Experience from "./Experience";
-import React from "react";
+import itmhPDF from "../assets/documents/Introduction to Music History-Kevin Tran.pdf";
 
 export default function Compositions() {
   return (
@@ -9,14 +8,25 @@ export default function Compositions() {
       <div className="m-auto max-w-250">
         <h1 className="font-lexend pb-5 text-center text-5xl font-semibold">Compositions</h1>
 
-        {CompositionData.map((value: ExperienceType, index) => (
-          <React.Fragment key={index}>
-            <Experience data={value} />
-
-            {index < CompositionData.length - 1 && <div className="h-0.5 w-full bg-black" />}
-          </React.Fragment>
-        ))}
+        <IntroductionToMusicHistory data={CompositionData[0]} />
       </div>
     </section>
+  );
+}
+
+function IntroductionToMusicHistory({ data }: { data: ExperienceType }) {
+  return (
+    <div className="mx-auto my-10 max-w-200">
+      <h1 className="font-lexend my-10 text-3xl">{data.title}</h1>
+      <div className="my-10 border">
+        <iframe className="h-120 w-full" src={data.videoLink} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+      </div>
+
+      <a href={itmhPDF} className="mb-8 inline-block rounded-xl border bg-white px-3 py-2 text-black hover:bg-purple-700 hover:text-white">
+        View PDF
+      </a>
+
+      <p className="font-outfit text-xl font-light" dangerouslySetInnerHTML={{ __html: data.description }}></p>
+    </div>
   );
 }
